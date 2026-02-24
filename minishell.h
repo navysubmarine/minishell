d8b64b86a7fabafe71e8ffd520b5f07f5b32b7dc
Gnoumagbe Camara <gcamara@f4r3s7.clusters.42paris.fr>
2026-02-24 11:22:36 +0100

    update minishell

--- minishell.h
+++ minishell.h
@@ -1,9 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minishell.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gcamara <gcamara@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/02/24 10:59:25 by gcamara           #+#    #+#             */
+/*   Updated: 2026/02/24 11:01:03 by gcamara          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
-/* to be able to use the readline function*/
-#include <readline/history.h>
-#include <readline/readline.h>
-#include <term.h>
-#include <curses.h>
-#include <stdio.h>
-#include <termios.h>
-#include <unistd.h>
+#ifndef MINISHELL_H
+# define MINISHELL_H
+
+# include <readline/history.h>
+# include <readline/readline.h>
+# include <term.h>
+# include <curses.h>
+# include <stdio.h>
+# include <termios.h>
+# include <sys/ioctl.h>
+# include <unistd.h> 
+# include <stdlib.h> 
+# include <sys/types.h>
+# include <dirent.h>
+# include <sys/stat.h>
+
+#endif
