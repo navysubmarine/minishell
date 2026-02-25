7b6a5c9edb528ee88eeaf378ce93488a7f6e50d6
marthoma <marthoma@student.42.fr>
2026-02-25 11:56:05 +0100

    correction makefile et debut readline

--- Marine/minishell.c
+++ Marine/minishell.c
@@ -6,29 +6,29 @@
 /*   By: marthoma <marthoma@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/23 10:24:05 by marthoma          #+#    #+#             */
-/*   Updated: 2026/02/23 16:19:00 by marthoma         ###   ########.fr       */
+/*   Updated: 2026/02/25 11:43:32 by marthoma         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-int	main(int argc, char **argv)
+int	main(void)
 {
-	// the current directory
 	char cwd[1024];
-	char *input;
+	char *rl;
 
-	input = argv[1];
 	// shell is running in interactive mode
 	if (isatty(STDIN_FILENO))
 	{
-		getcwd(cwd, sizeof(cwd));
-		
 		// accept input from user and execute commands
+		getcwd(cwd, sizeof(cwd));
+		rl = readline(cwd);
+		ft_printf("%s\n", rl);
 	}
 	// shell is running in non interactive mode
 	else
 	{
 		// execute commands from script
 	}
+	return (0);
 }
--- Marine/minishell.h
+++ Marine/minishell.h
@@ -6,10 +6,26 @@
 /*   By: marthoma <marthoma@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/23 17:11:38 by marthoma          #+#    #+#             */
-/*   Updated: 2026/02/23 17:12:50 by marthoma         ###   ########.fr       */
+/*   Updated: 2026/02/25 11:51:41 by marthoma         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-# include <unistd.h>
+#ifndef MINISHELL_H
+# define MINISHELL_H
+
+# include <readline/history.h>
+# include <readline/readline.h>
+# include <term.h>
+# include <curses.h>
 # include <stdio.h>
-# 
+# include <termios.h>
+# include <sys/ioctl.h>
+# include <unistd.h> 
+# include <stdlib.h> 
+# include <sys/types.h>
+# include <dirent.h>
+# include <sys/stat.h>
+# include "ft_printf.h"
+# include "libft.h"
+
+#endif
